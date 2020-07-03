6ae7796c344bcabf60c1fef33af0d439918999d4
Florestan Korp <florestankorp@gmail.com>
2020-07-03 21:15:59 +0200

    storing new entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,7 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from . import util
 
@@ -18,14 +21,43 @@ entry exists, the function returns None.
 """
 
 
+class EntryForm(forms.Form):
+    title = forms.CharField(label="New Entry")
+    content = forms.CharField(widget=forms.Textarea)
+
+
 def index(request):
     return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
 
 
 def entry(request, entry):
     e = util.get_entry(entry)
-    print(e)
     if e is None:
-        return render(request, "encyclopedia/error.html", {"entry": entry})
+        return render(
+            request, "encyclopedia/error.html", {"error": f"{entry} not found"}
+        )
 
     return render(request, "encyclopedia/entry.html", {"entry": e, "name": entry})
+
+
+def new(request):
+    if request.method == "POST":
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+
+            e = util.get_entry(title)
+            if e is None:
+                util.save_entry(title, content)
+            else:
+                return render(
+                    request,
+                    "encyclopedia/error.html",
+                    {"error": f"Encyclopedia entry {title} already exists"},
+                )
+
+            # validate validate validate
+            return HttpResponseRedirect(title)
+
+    return render(request, "encyclopedia/new.html", {"form": EntryForm()})
